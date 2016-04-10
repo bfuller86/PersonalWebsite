42d1c74834a14a997c33df1353e63b13ce8941eb
Brian Fuller <bfuller86@gmail.com>
2016-04-10 18:10:27 -0500

    Add favicon

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt){
-    
+
     // Project configuration.
     grunt.log.writeln("Configuring Gruntfile...");
     grunt.initConfig({
        pkg: grunt.file.readJSON('package.json'),
-       
+
        concat: {
          options: {
              separator: '\n'
@@ -21,15 +21,15 @@ module.exports = function(grunt){
        clean: {
           build: ['build/*']
        },
-       
+
        copy: {
            main: {
-               src: ['index.html', 'app.css', 'app/**/*{.html,.css,.json}'],
+               src: ['index.html', 'app.css', 'app/**/*{.html,.css,.json}', 'favicon.ico'],
                dest: 'build/'
            },
            devScripts: {
                src: ['app.js'],
-               dest: 'build/'  
+               dest: 'build/'
            },
            prodScripts: {
                src:['app.min.js'],
@@ -45,7 +45,7 @@ module.exports = function(grunt){
               dest: 'build/'
            }
        },
-       
+
        uglify: {
            options: {
                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -56,19 +56,19 @@ module.exports = function(grunt){
                dest: 'app.min.js'
            }
        },
-       
+
        sass: {
             dist: {
                files: {
                   'app.css': 'app/styles/app.scss'
                }
-            } 
+            }
        },
-       
+
        qunit: {
            files: ['test/**/*.html']
        },
-       
+
        jshint: {
            files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js'],
            options: {
@@ -80,13 +80,13 @@ module.exports = function(grunt){
                }
            }
        },
-       
+
        watch: {
           files: ['index.html', 'declarations.js', '<%= jshint.files %>', 'vendor/**/*.js', 'app/**/*{.js,.html,.scss}'],
           tasks: ['clean:build', 'sass', 'copy:main', 'jshint', 'concat', 'uglify:buildApp', /*'copy:prodScripts'*/ 'copy:devScripts']
        }
     });
-    
+
     // Load the plugins that provide the required tasks.
     grunt.log.writeln("Loading NPM tasks...");
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -97,16 +97,16 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    
+
     // Default task(s)
     grunt.log.writeln("Registering default Grunt task...");
     grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
-    
+
     grunt.registerTask('update-vendor', ['clean:vendor', 'copy:vendor']);
     grunt.registerTask('test', ['jshint', 'qunit']);
     grunt.registerTask('testcopy', ['clean:build', 'copy:main']);
     grunt.registerTask('localBuild', ['clean:build', 'copy:main', 'jshint', 'concat', 'uglify', 'copy:vendorLocalOnly']);
-    
+
     // Done!
     grunt.log.writeln("Gruntfile operations completed.");
 };
